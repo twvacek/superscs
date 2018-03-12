b45665eec960030687d4e99e61bad3c9e54f2fc6
Pantelis Sopasakis <p.sopasakis@gmail.com>
2018-03-13 00:57:23 +0100

    updated results db

--- tests/profiling_matlab/profile_runners/profile_runner_pca.m
+++ tests/profiling_matlab/profile_runners/profile_runner_pca.m
@@ -1,14 +1,14 @@
-% PCA PROBLEMS [PCA-1B]
+% PCA PROBLEMS [PCA-1B]e
 rng(1); % for reproducibility (so that every time this script is called,
 % the same problems will be run).
 records = [];
 reps = 2;
 k=0;
-for d=[50 100 150 200],
-    for p = [5 10],
+for d=[50 120 140 180],
+    for p = [5 8],
         for rca = logspace(-5,-2,3),
-            for lam = [0.1 2 10],
-                for dens = [0.1 0.5],
+            for lam = [0.1 2 5],
+                for dens = [0.05 0.1],
                     problem.d = d;
                     problem.p = p;
                     problem.density = dens;
@@ -17,7 +17,7 @@ for d=[50 100 150 200],
                     for r=1:reps,
                         k = k+1;
                         fprintf(...
-                            ['PCA PROBLEM #%d', ...
+                            ['PCA PROBLEM #%d ', ...
                             '[d=%d, p=%d, rca=%g, lam=%.1f, den=%.1f, rep=%d]\n'],...
                             k, d, p, rca, lam, dens, r);
                         profile_pca(problem, tol, o);
