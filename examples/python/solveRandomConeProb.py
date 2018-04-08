f00efe4de200be716e6453004e357d02177f1250
Brendan O'Donoghue <bodonoghue85@gmail.com>
2018-04-08 14:46:12 +0100

    update the python interface

--- examples/python/solveRandomConeProb.py
+++ examples/python/solveRandomConeProb.py
@@ -1,11 +1,11 @@
 from __future__ import print_function, division
-import scs
+import superscs
 from numpy import *
 from scipy import sparse, randn
 from genRandomConeProb import *
 
 #############################################
-#  Uses scs to solve a random cone problem  #
+#  Uses superscs to solve a random cone problem  #
 #############################################
 
 def main():
@@ -21,14 +21,14 @@ def solveFeasible():
     data, p_star = genFeasible(K, n = m//3, density = 0.01)
     params = {'eps':1e-3, 'normalize':True, 'scale':5, 'cg_rate':2}
     
-    sol_i = scs.solve(data, K, use_indirect=True, **params)
+    sol_i = superscs.solve(data, K, use_indirect=True, **params)
     xi = sol_i['x']
     yi = sol_i['y']
     print('p*  = ', p_star)
     print('pri error = ', (dot(data['c'], xi) - p_star) / p_star)
     print('dual error = ', (-dot(data['b'], yi) - p_star) / p_star)
     # direct:
-    sol_d = scs.solve(data, K, use_indirect=False, **params)
+    sol_d = superscs.solve(data, K, use_indirect=False, **params)
     xd = sol_d['x']
     yd = sol_d['y']
     print('p*  = ', p_star)
@@ -40,16 +40,16 @@ def solveInfeasible():
     m = getConeDims(K)
     data = genInfeasible(K, n = m//3)
     params = {'eps':1e-4, 'normalize':True, 'scale':0.5, 'cg_rate':2}
-    sol_i = scs.solve(data, K, use_indirect=True, **params)
-    sol_d = scs.solve(data, K, use_indirect=False, **params)
+    sol_i = superscs.solve(data, K, use_indirect=True, **params)
+    sol_d = superscs.solve(data, K, use_indirect=False, **params)
 
 def solveUnbounded():
     K = {'f':10, 'l':15, 'q':[5, 10, 0 ,1], 's':[3, 4, 0, 0, 1], 'ep':10, 'ed':10, 'p':[-0.25, 0.5, 0.75, -0.33]}
     m = getConeDims(K)
     data = genUnbounded(K, n = m//3)
     params = {'eps':1e-4, 'normalize':True, 'scale':0.5, 'cg_rate':2}
-    sol_i = scs.solve(data, K, use_indirect=True, **params)
-    sol_d = scs.solve(data, K, use_indirect=False, **params)
+    sol_i = superscs.solve(data, K, use_indirect=True, **params)
+    sol_d = superscs.solve(data, K, use_indirect=False, **params)
 
 if __name__ == "__main__":
    main()
