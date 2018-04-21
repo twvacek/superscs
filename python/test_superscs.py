e2a507105ed0f96823dad7b4c25eaa990f42e422
Pantelis Sopasakis <p.sopasakis@gmail.com>
2018-04-21 18:12:13 +0300

    :white_check_mark: python tests

--- python/test_superscs.py
+++ python/test_superscs.py
@@ -1,25 +1,60 @@
 import unittest
 import superscs
 import numpy as np
+from numpy import linalg as npla
 from scipy import sparse
 
 
-class TestStringMethods(unittest.TestCase):
-
+class BroydenTestCase(unittest.TestCase):
 
     def test_nothing(self):
-	a = 1
+	return
 
-    def test_superscs(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-	ij = np.array([[0,1,2,3],[0,1,2,3]])
-	A = sparse.csc_matrix(([-1.,-1.,1.,1.], ij), (4,4))
-	b = np.array([0.,0.,1,1])
-	c = np.array([1.,1.,-1,-1])
+    def test_superscs_solved(self):    
+	x_correct = np.array([0., 0., 1., 1.])   
+	ij = np.array([[0, 1, 2, 3] ,[0, 1, 2, 3]])
+	A = sparse.csc_matrix(([-1., -1., 1., 1.], ij), (4, 4))
+	b = np.array([0., 0.,  1,  1])
+	c = np.array([1., 1., -1, -1])
 	cone = {'l': 4}
-	data = {'A': A, 'b':b, 'c':c}
-	sol = superscs.solve(data, cone, use_indirect = False, memory = 50, verbose = 0)
+	data = {'A': A, 'b': b, 'c': c}
+	tolerance = 1e-8
+	sol = superscs.solve(\
+		data, cone, do_super_scs = True, \
+		direction = 100, \
+		use_indirect = False, memory = 10, \
+		verbose = 0, eps = tolerance)
+	self.assertEquals('Solved',sol['info']['status'])
+	self.assertEquals(1, sol['info']['statusVal'])
+	self.assertTrue(sol['info']['resPri'] < tolerance)
+	self.assertTrue(sol['info']['resDual'] < tolerance)
+	self.assertTrue(npla.norm(x_correct - sol['x']) < 1e-12)
 
+
+
+class AATestCase(unittest.TestCase):
+
+
+    def test_superscs_aa(self):    
+	x_correct = np.array([0., 0., 1., 1.])   
+	ij = np.array([[0, 1, 2, 3] ,[0, 1, 2, 3]])
+	A = sparse.csc_matrix(([-1., -1., 1., 1.], ij), (4, 4))
+	b = np.array([0., 0.,  1,  1])
+	c = np.array([1., 1., -1, -1])
+	cone = {'l': 4}
+	data = {'A': A, 'b': b, 'c': c}
+	tolerance = 1e-8
+	sol = superscs.solve(\
+		data, cone, do_super_scs = True, \
+		direction = 150, \
+		use_indirect = False, memory = 3, \
+		verbose = 0, eps = tolerance)
+	self.assertEquals('Solved',sol['info']['status'])
+	self.assertEquals(1, sol['info']['statusVal'])
+	self.assertTrue(sol['info']['resPri'] < tolerance)
+	self.assertTrue(sol['info']['resDual'] < tolerance)
+	self.assertTrue(npla.norm(x_correct - sol['x']) < 1e-12)
 
 if __name__ == '__main__':
     unittest.main()
+
