c5ad873a4dcda2b6b529d2350246d691256ad126
Pantelis Sopasakis <p.sopasakis@gmail.com>
2018-04-24 20:56:22 +0300

    :beetle: :memo: docs typos [ci skip]

--- tests/matlab/benchmark_lasso.m
+++ tests/matlab/benchmark_lasso.m
@@ -7,33 +7,25 @@ n = 1000; m = ceil(n/5); s=ceil(n/10);
 x_true=[randn(s,1);zeros(n-s,1)]; % true sparse signal
 x_true = x_true(randperm(n));
 
-density = 0.1;
-rcA = .01;
+density = 0.2;
+rcA = 1e-8;
 A=sprandn(m,n,density,rcA);
 
-b = A*x_true + 0.1*randn(m,1);
-mu = 1;
+b = A*x_true + 0.4*randn(m,1);
+mu = 15;
 
-do_super_scs = 1;
-
+scs_options = SuperSCSConfig.douglasRachfordConfig('tolerance', 1e-8,...
+    'do_record_progress',1,'memory',100);
 tic;
 cvx_begin
     cvx_solver scs
-    cvx_solver_settings('eps', 1e-7,...
-        'scale', 1,...
-        'do_super_scs',do_super_scs,...
-        'direction', 150,...
-        'k0', 0,...
-        'memory', 20,...
-        'rho_x', 0.001,...
-        'verbose', 2, ...
-        'do_record_progress',0, ...
-        'use_indirect',1)
-    cvx_solver_settings('dumpfile', 'lasso.mat');
+    set_pars(scs_options)
     variable x_c(n)
     minimize(0.5*sum_square(A*x_c - b) + mu*norm(x_c,1))
 cvx_end
 toc
-%load('lasso.mat')
-%semilogy(info.progress_time, info.progress_respri,'linewidth',2)
-%grid on
+
+
+load(scs_options.dumpfile)
+semilogy(info.progress_time/1e3, info.progress_respri)
+hold on
--- tests/matlab/benchmark_logreg.m
+++ tests/matlab/benchmark_logreg.m
@@ -1,35 +1,30 @@
-clear
-rng('default');
-rng(1);
-
-density = 0.1;
-
-p = 200; % features
-q = 100*p; % total samples
+density = 0.4;
+p = 100;   % features
+q = 10*p;   % total samples
 w_true = sprandn(p,1,0.2);
-
 X_tmp = 3*sprandn(p, q, density);
 ips = -w_true'*X_tmp;
 ps = (exp(ips)./(1 + exp(ips)))';
 labels = 2*(rand(q,1) < ps) - 1;
-
 X_pos = X_tmp(:,labels==1);
 X_neg = X_tmp(:,labels==-1);
-
 X = [X_pos -X_neg]; % include labels with data
+lam = .1; 
 
-lam = 2; 
-%%
-tic
+
+scs_options = SuperSCSConfig.douglasRachfordConfig('tolerance', 1e-4,...
+    'do_record_progress',1,'memory',100,'verbose',2);
+
+
 cvx_begin
-    cvx_solver scs
-    cvx_solver_settings('eps', 1e-3,...
-        'do_super_scs', 1,...
-        'direction', 100,...
-        'ls', 5,...
-        'memory', 50,...
-        'verbose', 2)
-    variable w(p)
-    minimize(sum(log_sum_exp([sparse(1,q);w'*X])) + lam * norm(w,1))        
+   cvx_solver scs
+   set_pars(scs_options);
+   variable w(p)
+   minimize(sum(log_sum_exp([sparse(1,q);w'*X])) + lam * norm(w,1))       
 cvx_end
-toc
+
+
+
+load(scs_options.dumpfile)
+semilogy(info.progress_time/1e3, info.progress_respri)
+hold on
