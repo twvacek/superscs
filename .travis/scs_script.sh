25ae2763fe2f25998e9a0c5948309a15fc7093a3
Pantelis Sopasakis <p.sopasakis@gmail.com>
2018-04-29 23:48:09 +0300

    :hammer: travis tests

--- .travis/scs_script.sh
+++ .travis/scs_script.sh
@@ -11,12 +11,14 @@ if [[ $CC = *"gcc"* ]] && [[ $SCS_RUN_PYTHON == 1 ]]; then
 fi
 
 # Run profiler on an easy problem (just to make sure it works)
-#if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-#    sudo make clean
-#    make profile-run PF=1 PROBLEM_YAML_FILE='\"tests/c/data/test-5.yml\"' -s
-#fi
+# (execute both on linux and on mac osx)
+sudo make clean
+make profile-run PF=1 PROBLEM_YAML_FILE='\"tests/c/data/test-5.yml\"' -s
+make profile-run PF=1 SCS_DIR=150 SCS_MEM=3 PROBLEM_YAML_FILE='\"tests/c/data/test-5.yml\"' -s
+make profile-run PF=1 SCS_DIR=100 SCS_MEM=20 PROBLEM_YAML_FILE='\"tests/c/data/test-5.yml\"' -s
 
 # Run coverage (only for gcc)
 if [[ $CC = *"gcc"* ]] && [[ $SCS_RUN_COV == 1 ]]; then 
+    sudo make clean;
     make COV=1 cov; 
 fi
