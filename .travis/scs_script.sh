648b2f40a96c8bb45c0b575c5b505b92e8b3880c
Pantelis Sopasakis <p.sopasakis@gmail.com>
2018-04-29 11:21:58 +0300

    :hammer: osx travis issues

--- .travis/scs_script.sh
+++ .travis/scs_script.sh
@@ -11,7 +11,7 @@ if [[ $CC = *"gcc"* ]] && [[ $SCS_RUN_PYTHON == 1 ]]; then
 fi
 
 # Run profiler on an easy problem (just to make sure it works)
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo make clean
     make profile-run PF=1 PROBLEM_YAML_FILE='\"tests/c/data/test-5.yml\"' -s
 fi
